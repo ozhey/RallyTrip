978de337143069126f46949b60b35e459d5f81b5
Oz <ozhey119@gmail.com>
2022-01-28 10:26:51 +0200

    add airbag vests category + make root path const

--- src/links.js
+++ src/links.js
@@ -1,38 +1,42 @@
+const PRODUCTS_ROOT = "/products/"
 const links = [
     {
-        path: '/products/roadbooks', name: 'סיפורי דרך', routes : []
+        path: PRODUCTS_ROOT + 'roadbooks', name: 'סיפורי דרך', routes : []
     },
     {
-        path: '/products/roadbook-holders', name: 'ספרי דרך', routes: [
-            { path: '/products/roadbook-holders/accessories', name: 'אביזרים נלווים' },
-            { path: '/products/roadbook-holders/mounts', name: 'תושבות' },
-            { path: '/products/roadbook-holders/holders', name: 'ספרים' },
+        path: PRODUCTS_ROOT + 'roadbook-holders', name: 'ספרי דרך', routes: [
+            { path: PRODUCTS_ROOT + 'roadbook-holders/accessories', name: 'אביזרים נלווים' },
+            { path: PRODUCTS_ROOT + 'roadbook-holders/mounts', name: 'תושבות' },
+            { path: PRODUCTS_ROOT + 'roadbook-holders/holders', name: 'ספרים' },
         ]
     },
     {
-        path: '/products/icos', name: 'מדי מרחק', routes : []
+        path: PRODUCTS_ROOT + 'icos', name: 'מדי מרחק', routes : []
     },
     {
-        path: '/products/garmin', name: 'גרמין', routes: [
-            { path: '/products/garmin/watches', name: 'שעונים' },
-            { path: '/products/garmin/devices', name: 'מכשירים' },
-            { path: '/products/garmin/garmin-mounts', name: 'תושבות' }
+        path: PRODUCTS_ROOT + 'garmin', name: 'גרמין', routes: [
+            { path: PRODUCTS_ROOT + 'garmin/watches', name: 'שעונים' },
+            { path: PRODUCTS_ROOT + 'garmin/devices', name: 'מכשירים' },
+            { path: PRODUCTS_ROOT + 'garmin/garmin-mounts', name: 'תושבות' }
         ]
     },
     {
-        path: '/products/communicators', name: 'דיבוריות', routes : []
+        path: PRODUCTS_ROOT + 'communicators', name: 'דיבוריות', routes : []
     },
     // {
-    //     path: '/products/towers', name: 'מגדלי ניווט', routes : []
+    //     path: PRODUCTS_ROOT + 'towers', name: 'מגדלי ניווט', routes : []
     // },
     {
-        path: '/products/wheels', name: 'גלגלים', routes : []
+        path: PRODUCTS_ROOT + 'wheels', name: 'גלגלים', routes : []
     },
     {
-        path: '/products/grips', name: 'גריפים', routes : []
+        path: PRODUCTS_ROOT + 'grips', name: 'גריפים', routes : []
     },
     {
-        path: '/products/gopro', name: 'GoPro', routes : []
+        path: PRODUCTS_ROOT + 'gopro', name: 'GoPro', routes : []
+    },
+    {
+        path: PRODUCTS_ROOT + 'airbag-vests', name: 'אפוד מתנפח', routes : []
     }
 ]
 
